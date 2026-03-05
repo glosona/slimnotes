77a0bf08edfbeb16ed644d6f75e57d1652a6aa8a
glosona <sonaze28@gmail.com>
2026-03-05 14:51:00 +0900

    Initial Commit

---
+++ SlimNotes/SlimNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlimNotesApp.swift
+//  SlimNotes
+//
+//  Created by Son Hayoung on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SlimNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlimNotesTests/SlimNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlimNotesTests.swift
+//  SlimNotesTests
+//
+//  Created by Son Hayoung on 3/5/26.
+//
+
+import Testing
+@testable import SlimNotes
+
+struct SlimNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
